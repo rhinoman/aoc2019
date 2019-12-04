2ba2d4c6388e56ebc614df7e52abefe6017e4c8b
James Adam <james.adam.ctr@isso.net>
2019-12-04 07:44:03 -0500

    day 4 solved

--- src/aoc2019/day4.clj
+++ src/aoc2019/day4.clj
@@ -18,20 +18,31 @@
   (let [solutions (keep (fn [x] (if (is-solution? x) x nil)) sp)]
     (count solutions)))
 
+(defn add-run [x nx prev runs]
+  (cond
+    (= x nx prev) (assoc runs x 3)
+    (= x nx) (assoc runs x 2)
+    :else runs))
+
+(defn eval-runs [runs]
+  (> (count (filter (fn [x] (= 2 (get x 1))) runs)) 0))
+
 (defn is-solution-2? [x]
   (let [xstr (str x)]
     (loop [xs xstr
-           has-dbl 0
+           runs {}
+           prev nil
            is-inc true]
       (let [cur (first xs)
-            nx (second xs)
-            tr (second (rest xs))
-            dbl (if (= cur nx) 1 0)
-            tpl (if (= cur nx tr) -2 0)]
+            nx (second xs)]
         (cond
-          (nil? nx) (and is-inc (> has-dbl 0))
+          (nil? nx) (and is-inc (eval-runs runs))
           (< (Character/digit nx 10) (Character/digit cur 10)) false
-          :else (recur (rest xs) (+ has-dbl dbl tpl) is-inc))))))
+          :else (recur
+                 (rest xs)
+                 (add-run cur nx prev runs)
+                 cur
+                 is-inc))))))
 
 (defn solve-part-2 []
   (let [solutions (keep (fn [x] (if (is-solution-2? x) x nil)) sp)]
